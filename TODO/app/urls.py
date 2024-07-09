c9220010d4de9191d7f9c521461391a3bdfe20b8
bansodevaibhav032 <129043443+bansodevaibhav032@users.noreply.github.com>
2024-07-10 00:11:32 +0530

    create form for signup and the signup functionality.

--- TODO/app/urls.py
+++ TODO/app/urls.py
@@ -2,9 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
-from app.views import home
+from app.views import home,login,signup
  
 
 urlpatterns = [
-    path('',home)
+    path('',home,name='home'),
+    path('login/',login,name='login'),
+    path('signup/',signup,name='signup'),
+    
 ]
--- TODO/app/views.py
+++ TODO/app/views.py
@@ -1,8 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
     
     return render(request, 'index.html')
 
+def login(request):
+    
+    return render(request, 'login.html')
+
+def signup(request):
+    if request.method == 'GET':
+        form= UserCreationForm()    
+        context ={
+            'form':form
+
+        }
+        return render(request, 'signup.html',context=context)
+    else:
+        print(request.POST)
+        form= UserCreationForm(request.POST)
+        context ={
+            'form':form
+
+        }
+        if form.is_valid():
+            user=form.save()
+            print(user)
+            if user is not None:
+                return redirect('home')
+        else:
+            return render(request, 'signup.html',context=context)
