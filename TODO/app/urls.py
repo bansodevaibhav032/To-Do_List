1e0e62f90637dee904d98b4525a9acf7f89bd023
bansodevaibhav032 <129043443+bansodevaibhav032@users.noreply.github.com>
2024-07-10 17:08:22 +0530

    login page functionality added.

--- TODO/app/urls.py
+++ TODO/app/urls.py
@@ -2,12 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
-from app.views import home,login,signup
+from app.views import home,login,signup,add_todo
  
 
 urlpatterns = [
     path('',home,name='home'),
     path('login/',login,name='login'),
     path('signup/',signup,name='signup'),
-    
+    path('add-todo/',add_todo,name='add_todo'),
 ]
--- TODO/app/views.py
+++ TODO/app/views.py
@@ -1,16 +1,38 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login as loginUser
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from app.forms import TODOForm
+from app.models import TODO
 
 # Create your views here.
 def home(request):
-    
-    return render(request, 'index.html')
+    form = TODOForm()
+    todos = TODO.objects.all() 
+    return render(request, 'index.html',context={'form':form, 'todos':todos})
 
 def login(request):
-    
-    return render(request, 'login.html')
-
+    if request.method == 'GET':
+        form=AuthenticationForm()
+        context={
+            'form':form
+        }
+        return render(request, 'login.html',context=context)
+    else:
+        form=AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username= form.cleaned_data.get('username')
+            password= form.cleaned_data.get('password')
+            user=authenticate(username=username, password=password)
+            if user is not None:
+                loginUser(request,user)
+                return redirect("home")
+        else:
+            context={
+                'form':form
+            }
+            return render(request, 'login.html',context=context)
+            
 def signup(request):
     if request.method == 'GET':
         form= UserCreationForm()    
@@ -33,3 +55,22 @@ def signup(request):
                 return redirect('home')
         else:
             return render(request, 'signup.html',context=context)
+        
+def logout(request):
+    pass
+
+def add_todo(request):
+    if request.user.is_authenticated:
+        user=request.user
+        print(user)
+        form= TODOForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            todo=form.save(commit=False)
+            todo.user=user
+            todo.save()
+            print(todo)
+            return redirect("home")
+        else:
+            return render(request, 'index.html',context={'form':form})
+
